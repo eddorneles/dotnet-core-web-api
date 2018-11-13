23383f63a2df780c85f338cfdc0298cd3b71b90d
Eduardo Dorneles <eduardo.dorneles.fs@gmail.com>
2018-11-12 22:58:47 -0300

    Criação da classe Model Person

---
+++ Model/Person.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace webapi01.Model{
+    public class Person{
+        public long Id{ get, set};
+    }//END class
+
+}//END namespace
