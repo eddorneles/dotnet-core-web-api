9a4b6e0b09c1879fecd67f9e0909efb066fa37d0
Eduardo Dorneles <eduardo.dorneles.fs@gmail.com>
2018-11-12 23:06:06 -0300

    Atualização dos arquivos a serem ignorados

--- Model/Person.cs
+++ Model/Person.cs
@@ -4,9 +4,16 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-namespace webapi01.Model{
-    public class Person{
-        public long Id{ get, set};
+namespace webapi01.Model
+{
+    public class Person
+    {
+        public long Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string Gender { get; set; }
+
     }//END class
 
 }//END namespace
