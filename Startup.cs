104653557a67c49b925edbf6d23a9ff9d5b24d21
Eduardo Dorneles <eduardo.dorneles.fs@gmail.com>
2018-11-13 01:39:30 -0300

    conclusão dos preparativos para conexão com o banco de dados

--- Startup.cs
+++ Startup.cs
@@ -11,23 +11,27 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
+using Microsoft.EntityFrameworkCore;
+
 using webapi01.Services;
 using webapi01.Services.Implementations;
+using webapi01.Model.Context;
 
 namespace webapi01
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
+        public Startup(IConfiguration configuration){
+            this.Configuration = configuration;
+        }//END consructor
 
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            var connection = Configuration["MySqlConnection:MySqlConnectionString"];
+            services.AddDbContext<MySQLContext>( options => options.UseMySql( connection ) );
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
             //Injeção de dependência
             services.AddScoped<IPersonService, PersonServiceImpl>();
